f3cdcdc9313de009a0b50d717af4c531c3f353da
Der-Alex <alexander.thimm@googlemail.com>
2020-06-01 23:32:10 +0200

    default stylings - added default scss structure with minimal styling - added test page to see default styling

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="top" class="page" role="document">
     <header role="banner">
-      <h1>HTML5 Test Page</h1>
+      <h1 class="stage">HTML5 Test Page</h1>
       <p>This is a test page filled with common HTML elements to be used to provide visual feedback whilst building CSS systems and frameworks.</p>
     </header>
     <nav role="navigation">
@@ -49,15 +49,15 @@
     </nav>
     <main role="main">
       <section id="text">
-        <header><h1>Text</h1></header>
+        <header><h1 class="stage">Text</h1></header>
         <article id="text__headings">
           <header>
-            <h1>Headings</h1>
+            <h1 class="stage">Headings</h1>
           </header>
           <div>
-            <h1>Heading 1</h1>
-            <h2>Heading 2</h2>
-            <h3>Heading 3</h3>
+            <h1 class="h1">Heading 1</h1>
+            <h2 class="h2">Heading 2</h2>
+            <h3 class="h3">Heading 3</h3>
             <h4>Heading 4</h4>
             <h5>Heading 5</h5>
             <h6>Heading 6</h6>
@@ -65,14 +65,14 @@
           <footer><p><a href="#top">[Top]</a></p></footer>
         </article>
         <article id="text__paragraphs">
-          <header><h1>Paragraphs</h1></header>
+          <header><h1 class="stage">Paragraphs</h1></header>
           <div>
             <p>A paragraph (from the Greek paragraphos, “to write beside” or “written beside”) is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.</p>
           </div>
           <footer><p><a href="#top">[Top]</a></p></footer>
         </article>
         <article id="text__blockquotes">
-          <header><h1>Blockquotes</h1></header>
+          <header><h1 class="stage">Blockquotes</h1></header>
           <div>
             <blockquote>
               <p>A block quotation (also known as a long quotation or extract) is a quotation in a written document, that is set off from the main text as a paragraph, or block of text.</p>
@@ -83,7 +83,7 @@
           <footer><p><a href="#top">[Top]</a></p></footer>
         </article>
         <article id="text__lists">
-          <header><h1>Lists</h1></header>
+          <header><h1 class="stage">Lists</h1></header>
           <div>
             <h3>Definition list</h3>
             <dl>
@@ -106,14 +106,14 @@
           <footer><p><a href="#top">[Top]</a></p></footer>
         </article>
         <article id="text__hr">
-          <header><h1>Horizontal rules</h1></header>
+          <header><h1 class="stage">Horizontal rules</h1></header>
           <div>
             <hr>
           </div>
           <footer><p><a href="#top">[Top]</a></p></footer>
         </article>
         <article id="text__tables">
-          <header><h1>Tabular data</h1></header>
+          <header><h1 class="stage">Tabular data</h1></header>
           <table>
             <caption>Table Caption</caption>
             <thead>
@@ -168,7 +168,7 @@
           <footer><p><a href="#top">[Top]</a></p></footer>
         </article>
         <article id="text__code">
-          <header><h1>Code</h1></header>
+          <header><h1 class="stage">Code</h1></header>
           <div>
             <p><strong>Keyboard input:</strong> <kbd>Cmd</kbd></p>
             <p><strong>Inline code:</strong> <code>&lt;div&gt;code&lt;/div&gt;</code></p>
@@ -185,7 +185,7 @@
           <footer><p><a href="#top">[Top]</a></p></footer>
         </article>
         <article id="text__inline">
-          <header><h1>Inline elements</h1></header>
+          <header><h1 class="stage">Inline elements</h1></header>
           <div>
             <p><a href="#!">This is a text link</a>.</p>
             <p><strong>Strong is used to indicate strong importance.</strong></p>
@@ -209,7 +209,7 @@
           <footer><p><a href="#top">[Top]</a></p></footer>
         </article>
         <article id="text__comments">
-          <header><h1>HTML Comments</h1></header>
+          <header><h1 class="stage">HTML Comments</h1></header>
           <div>
             <p>There is comment here: <!--This comment should not be displayed--></p>
             <p>There is a comment spanning multiple tags and lines below here.</p>
@@ -221,7 +221,7 @@
         </article>
       </section>
       <section id="embedded">
-        <header><h1>Embedded content</h1></header>
+        <header><h1 class="stage">Embedded content</h1></header>
         <article id="embedded__images">
           <header><h2>Images</h2></header>
           <div>
@@ -274,7 +274,7 @@
         </article>
       </section>
       <section id="forms">
-        <header><h1>Form elements</h1></header>
+        <header><h1 class="stage">Form elements</h1></header>
         <form>
           <fieldset id="forms__input">
             <legend>Input fields</legend>

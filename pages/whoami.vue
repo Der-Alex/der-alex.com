a196b120e21211bed015fa224fdb7fd79b5aa882
Der Alex <alexander.thimm@googlemail.com>
2024-03-18 11:38:02 +0100

    add whoami page

---
+++ pages/whoami.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full max-w-5xl px-4 mx-auto wrapper">
+    <AwArticleDetail class="flex flex-col gap-4 text-rhino-100">
+      <p>Das ist noch geheim</p>
+    </AwArticleDetail>
+  </div>
+</template>

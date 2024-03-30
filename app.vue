a5c1d0392f4a5d3d07a42e27bcc06c80b65a5968
Der Alex <alexander.thimm@googlemail.com>
2024-03-30 16:11:32 +0100

    seo changes

--- app.vue
+++ app.vue
@@ -15,16 +15,15 @@ useHead(() => ({
       rel: 'favicon',
       href: 'favicon.ico',
       type: 'image/x-icon'
-  }
+    }
   ]
 }))
 
 </script>
 <template>
   <Html class="dark" lang="de" />
-  <Body
-    class="bg-rhino-950"
-    :class="{ 'mobile-menu-open': burgerStore.checked }" />
+
+  <Body class="bg-rhino-950" :class="{ 'mobile-menu-open': burgerStore.checked }" />
   <div class="is-wrapper max-w-5xl mx-auto">
     <MainNav class="hidden lg:block w-full max-w-5xl px-4 mx-auto" />
     <MobileNav />
@@ -33,9 +32,10 @@ useHead(() => ({
   </div>
 </template>
 <style>
-[data-content-id] > :first-child {
+[data-content-id]> :first-child {
   margin-top: 0 !important;
 }
+
 code,
 p strong:not(.reset),
 li strong {
@@ -43,57 +43,71 @@ li strong {
   border-radius: 4px;
   @apply bg-rhino-950;
 }
+
 footer strong {
   background-color: initial;
 }
+
 pre code {
   background: initial;
 }
+
 a:not(.article-link):not(.footer-link):not(.nav-link) {
   @apply text-amber-400 hover:text-amber-500;
 }
+
 table {
   table-layout: fixed;
   border-collapse: collapse;
   @apply rounded-3xl border border-rhino-700;
 }
+
 th,
 td {
   @apply border border-rhino-700 px-4 py-2;
 }
+
 .article-content {
   h2:not(.reset) {
     @apply text-rhino-400 text-xl font-bold mt-8 mb-4;
   }
+
   h3:not(.reset) {
     @apply text-rhino-400 text-base font-bold mt-6 mb-2;
   }
+
   ul:not(.list-none) li {
     @apply pl-4 relative;
+
     &::before {
       content: ' ';
       @apply absolute left-0 top-2 rounded-full w-2 h-2 bg-rhino-400;
     }
   }
 }
+
 .page-enter-active,
 .page-leave-active {
   transition: all 0.125s;
 }
+
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
   filter: blur(1rem);
 }
+
 .is-wrapper {
   min-height: 100dvh;
   display: grid;
   grid-template-columns: 100%;
   grid-template-rows: auto 1fr auto;
 }
+
 #__nuxt {
   min-height: 100dvh;
 }
+
 @media screen and (max-width: 1023px) {
   .mobile-menu-open {
     height: 100dvh;
